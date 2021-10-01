49922efae20d4f1602a94958d44c2d2254ad0098
Vinicius de Camargo Barbieri <barbierivc66@gmail.com>
2021-10-01 08:34:43 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Teste de Aplicativo E-commerce</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
